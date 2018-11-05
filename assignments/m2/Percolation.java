c15930e2b9a7d0a4476dc215fce55ca0596ce57b
saitejasedate <saitejavelagandula@msitprogram.net>
2018-11-05 23:50:04 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m2/Percolation.java
+++ assignments/m2/Percolation.java
@@ -6,30 +6,12 @@ import java.util.*;
  * Class for percolation.
  */
 class Percolation {
-	/**
-	 * int openSiteCount.
-	 */
     int openSiteCount;
-    /**
-     * boolean two- simensional array.
-     */
     boolean[][] arr;
-    /**
-     * Graph graph.
-     */
     Graph graph;
-    /**
-     * int n;
-     */
     int n;
 
     // create n-by-n grid, with all sites blocked
-
-    /**
-     * Constructs the object.
-     *
-     * @param      n     { parameter_description }
-     */
     public Percolation(int n) {
         this.n = n;
         arr = new boolean[n][n];
@@ -37,64 +19,37 @@ class Percolation {
    }
 
    // open site (row, col) if it is not open already
-
-   /**
-    * { function_description }
-    *
-    * @param      i     { parameter_description }
-    * @param      j     { parameter_description }
-    */
    public void open(int i, int j) {
         arr[i][j] = true;
 
         // Top site.
-        if(i == 0) {
-        	graph.addEdge(n*n, cal(i,j));
-        }
+        if(i == 0) graph.addEdge(n*n, cal(i,j));
 
         // Bottom site.
-        if(i == n-1) {
-        	graph.addEdge(n*n+1, cal(i,j));
-        }
+        if(i == n-1) graph.addEdge(n*n+1, cal(i,j));
 
         // bottom site
-        if (i < n-1 && arr[i+1][j] == true) {
+        if (i < n-1 && arr[i+1][j] == true)
             graph.addEdge(cal(i, j), cal(i+1, j));
-        }
 
         // top site
-        if (i > 0   && arr[i-1][j] == true) {
+        if (i > 0   && arr[i-1][j] == true)
             graph.addEdge(cal(i, j), cal(i-1, j));
-        }
 
         // right site
         if (j < n-1 && arr[i][j+1] == true)
             graph.addEdge(cal(i, j), cal(i, j+1));
 
         // left site
-        if (j > 0   && arr[i][j-1] == true){
+        if (j > 0   && arr[i][j-1] == true)
             graph.addEdge(cal(i, j), cal(i, j-1));
-        }
    }
-   /**
-    * { function_description }
-    *
-    * @param      i     { parameter_description }
-    * @param      j     { parameter_description }
-    *
-    * @return     { description_of_the_return_value }
-    */
+
    public int cal(int i, int j) {
         return (n*i)+j;
    }
 
    // does the system Percolates?
-
-   /**
-    * { function_description }
-    *
-    * @return     { description_of_the_return_value }
-    */
    public boolean percolates() {
         CC cc = new CC(graph);
         if (cc.connected(n*n, n*n+1))
