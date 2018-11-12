a9fe8fe4030dc8b359f2c90fe80045d6be91c816
saitejasedate <saitejavelagandula@msitprogram.net>
2018-11-12 12:33:52 +0530

    "ads2_m13_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m13/Radix Sorts/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * @author : saitejasedate.
+ * import java Scanner class.
+ */
+import java.util.*;
+/**
+ * Class for solution.
+ */
+public class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		/**
+		 * Empty Comstructor,
+		 */
+	}
+	/**
+     * Reads in a sequence of fixed-length strings from standard input;
+     * 3-way radix quicksorts them;
+     * and prints them to standard output in ascending order.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // read in the strings from standard input
+        Scanner sc = new Scanner(System.in);
+        int numOfStrings = sc.nextInt();
+        sc.nextLine();
+        Quick3string quick3way = new Quick3string();
+        String[] a = StdIn.readAllStrings();
+        // int n = a.length;
+
+        // sort the strings
+        quick3way.sort(a);
+
+        // print the results
+        for (int i = 0; i < numOfStrings; i++)
+            StdOut.println(a[i]);
+    }
+}
