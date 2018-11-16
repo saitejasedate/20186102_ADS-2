d94a92ccab29220ef1a7b253932af5651992107d
saitejasedate <saitejavelagandula@msitprogram.net>
2018-11-16 10:36:24 +0530

    "ads2_m16_a1_1: 0/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m16/boggle/BoggleSolver.java
+++ assignments/m16/boggle/BoggleSolver.java
@@ -1,43 +1,46 @@
 public class BoggleSolver {
-	TST<Integer>tst = new TST<Integer>();
+	TrieST<Integer>trie = new TrieST<Integer>();
 	// Initializes the data structure using the given array of strings as the dictionary.
 	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
 	public BoggleSolver(String[] dictionary) {
 		for (int i = 0; i < dictionary.length; i++) {
-			if (dictionary[i].length() > 3 || dictionary[i].length() < 4) {
-				tst.put(dictionary[i], 1);
+			if (dictionary[i].length() >= 3 || dictionary[i].length() <= 4) {
+				trie.put(dictionary[i], 1);
 			}
 			else if (dictionary[i].length() == 5) {
-				tst.put(dictionary[i], 2);
+				trie.put(dictionary[i], 2);
 			}
 			else if (dictionary[i].length() == 6) {
-				tst.put(dictionary[i], 3);
+				trie.put(dictionary[i], 3);
 			}
 			else if (dictionary[i].length() == 7) {
-				tst.put(dictionary[i], 5);
+				trie.put(dictionary[i], 5);
 			}
 			else if(dictionary[i].length() >= 8) {
-				tst.put(dictionary[i], 11);
+				trie.put(dictionary[i], 11);
 			}
-			// else {
-			// 	tst.put(dictionary[i], 0);
-			// }
+			else {
+				trie.put(dictionary[i], 0);
+			}
 		}
 	}
 
 	// Returns the set of all valid words in the given Boggle board, as an Iterable.
 	public Iterable<String> getAllValidWords(BoggleBoard board) {
+		if (board == null) {
+			throw new IllegalArgumentException("board is null");
+		}
 		return new Bag<String>();
 	}
 
 	// Returns the score of the given word if it is in the dictionary, zero otherwise.
 	// (You can assume the word contains only the uppercase letters A through Z.)
 	public int scoreOf(String word) {
-		if (tst.contains(word) == false) {
+		if (trie.contains(word) == false) {
 			return 0;
 		}
 		else {
-			return tst.get(word);
+			return trie.get(word);
 		}
 	}
 }
--- assignments/m16/boggle/Solution.java
+++ assignments/m16/boggle/Solution.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 /**
  * Class for solution.
  */
