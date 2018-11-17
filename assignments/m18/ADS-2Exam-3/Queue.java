a6174b1c02e1d2917637023bd5fdfc6090a8e6f0
saitejasedate <saitejavelagandula@msitprogram.net>
2018-11-17 13:08:19 +0530

    "ads2_m18_a1_1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m18/ADS-2Exam-3/Queue.java
+++ assignments/m18/ADS-2Exam-3/Queue.java
@@ -1,123 +1,110 @@
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-public class Queue<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of queue
-    private Node<Item> last;     // end of queue
-    private int n;               // number of elements on queue
+/**
+ * List of Queue.
+ *
+ * @param      <Item>  The item
+ */
+public class Queue<Item> {
+    /**
+     * {number of elements on queue}.
+     */
+    private int n;
+    /**
+     * {beginning of queue}.
+     */
+    private Node first;
+    /**
+     * {end of queue}.
+     */
+    private Node last;
 
-    // helper linked list class
-    private static class Node<Item> {
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * {Item}.
+         */
         private Item item;
-        private Node<Item> next;
+        /**
+         * {Node of type next}.
+         */
+        private Node next;
     }
 
     /**
-     * Initializes an empty queue.
+     * Create an empty queue.
      */
-    public Queue() {
+    Queue() {
         first = null;
         last  = null;
-        n = 0;
     }
 
     /**
-     * Returns true if this queue is empty.
-     * Time complexity is constant as the statement is iterated only once.
-     * @return {@code true} if this queue is empty; {@code false} otherwise
+     * Is the queue empty?
+     * @return     {Boolean}
      */
     public boolean isEmpty() {
         return first == null;
     }
 
     /**
-     * Returns the number of items in this queue.
-     * Time complexity is constant as the statement is iterated only once.
-     * @return the number of items in this queue
+     * Return the number of items in the queue.
+     * @return     {Integer}
      */
     public int size() {
         return n;
     }
 
     /**
-     * Returns the item least recently added to this queue.
-     * Time complexity is constant.
-     * @return the item least recently added to this queue
-     * @throws NoSuchElementException if this queue is empty
+     * Return the item least recently added to the queue.
+     * Throw an exception if the queue is empty.
+     * @return     {Item}
      */
     public Item peek() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        if (isEmpty()) {
+            throw new RuntimeException("Queue underflow");
+        }
         return first.item;
     }
 
+
     /**
-     * Adds the item to this queue.
-     * Time complexity is constant.
-     * @param  item the item to add
+     * {Add the item to the queue}.
+     *
+     * @param      item  The item
      */
-    public void enqueue(Item item) {
-        Node<Item> oldlast = last;
-        last = new Node<Item>();
-        last.item = item;
-        last.next = null;
-        if (isEmpty()) first = last;
-        else           oldlast.next = last;
+    public void enqueue(final Item item) {
+        Node x = new Node();
+        x.item = item;
+        if (isEmpty()) {
+            first = x;
+            last = x;
+        } else {
+            last.next = x;
+            last = x;
+        }
         n++;
     }
 
     /**
-     * Removes and returns the item on this queue that was least recently added.
-     * Time complexity is constant.
-     * @return the item on this queue that was least recently added
-     * @throws NoSuchElementException if this queue is empty
+     * Remove and return the item on the queue least recently added.
+     * Throw an exception if the queue is empty.
+     * @return     {Item}
      */
     public Item dequeue() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        if (isEmpty()) {
+            throw new RuntimeException(
+                "Queue underflow");
+        }
         Item item = first.item;
         first = first.next;
         n--;
-        if (isEmpty()) last = null;   // to avoid loitering
+        // to avoid loitering
+        if (isEmpty()) {
+            last = null;
+        }
         return item;
     }
-
-    /**
-     * Returns a string representation of this queue.
-     * Time complexity is N, as in the for-each it iterates over every element.
-     * @return the sequence of items in FIFO order, separated by spaces
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Item item : this) {
-            s.append(item);
-            s.append(' ');
-        }
-        return s.toString();
-    } 
-
-    /**
-     * Returns an iterator that iterates over the items in this queue in FIFO order.
-     * Time complexity is N as it iterates till the end.
-     * @return an iterator that iterates over the items in this queue in FIFO order
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
-    }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
-        }
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
-    }
 }
+
+
